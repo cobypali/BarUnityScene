7bee3e961b685bcd2f258a7d7ace0c996bd56e02
Coby Palivathukal <cobypali@gmail.com>
2020-07-23 10:30:34 -0700

    Character now has AnimationTree

---
+++ Assets/People/Martha T-Pose/AnimationStateTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateTrigger : MonoBehaviour
+{
+    public Animator charAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //running
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            charAnim.SetTrigger("Running");
+        }
+        //tpose
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            charAnim.SetTrigger("idle");
+        }
+        //running jump
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            charAnim.SetTrigger("RunningJump");
+        }
+        //walking
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            charAnim.SetTrigger("Walking");
+        }
+    }
+}
